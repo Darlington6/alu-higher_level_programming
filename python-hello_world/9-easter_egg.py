7c88d8c5ebcfa7ec6fe841367a75b0eb19470243
Darlington6 <d.tunyinko@alustudent.com>
2024-02-22 10:45:16 -0800

    Commit 9

---
+++ python-hello_world/9-easter_egg.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+import this
+```[^1^][1]
+
+This concise Python script prints "The Zen of Python" by Tim Peters, followed by a new line. The `import this` statement reveals the aphorisms that guide Python's design principles.
