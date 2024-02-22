240afcb21037afb3cdd4f3e9cbab89b795d2bc01
Darlington6 <d.tunyinko@alustudent.com>
2024-02-22 11:51:12 -0800

    Commit 8

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-sentence = "Python is an interpreted, interactive, object-oriented programming\
+str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-final = (sentence[39:66] + sentence[106:112 ] + sentence[:6])
-print(final)
+str = (str[39:66] + str[106:112 ] + str[:6])
+print(str)
