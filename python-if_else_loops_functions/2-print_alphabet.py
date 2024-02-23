9d436fc82021e970057dcc174a10516fe034f14f
Darlington6 <d.tunyinko@alustudent.com>
2024-02-23 02:50:20 -0800

    Commit 2

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for char in range(ord('a'), ord('z') + 1):
-    print(chr(char_code))
+alphabet = ""
+for char_code in range(ord('a'), ord('z') + 1):
+    alphabet += chr(char_code)
+print(alphabet)
+
