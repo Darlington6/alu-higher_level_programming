bcb6a19a00681a6659d0d53e1ad43f8f4b50a643
Darlington6 <d.tunyinko@alustudent.com>
2024-03-08 00:33:38 -0800

    Commit zero

---
+++ python-classes/0-square.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""Create a class called 'sqaure' which is empty"""
+
+class Sqaure:
+    pass
---
+++ python-classes/README.md
@@ -0,0 +1,1 @@
+This directory contains files with code of Python-Classes and Objects
