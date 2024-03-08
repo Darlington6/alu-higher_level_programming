bd6582fb3cbd7766f3ed6a78cd737b5e8857b4e4
Darlington6 <d.tunyinko@alustudent.com>
2024-03-08 00:54:27 -0800

    Commit 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-    #Create a square
+"""Create a square """
 
 
 class Square:
-    
-    #Create a square
-        #Has a private Instance att: size
-    
+    '''
+    Create a square
+        Has a private Instance att: size
+    '''
 
     def __init__(self, size=0):
-            #init size 
+        ''' init size '''
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
