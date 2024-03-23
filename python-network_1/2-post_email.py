86ccc6dcb98ceeab21fc190383ead00d6b353638
Darlington6 <d.tunyinko@alustudent.com>
2024-03-23 03:07:25 -0700

    Commit 2

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module is documented."""
+
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == '__main__':
+    """This is documented."""
+    url = sys.argv[1]
+    values = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii') # Data should be in bytes.
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        content = response.read()
+        print("{}".format(content.decode("utf-8")))
