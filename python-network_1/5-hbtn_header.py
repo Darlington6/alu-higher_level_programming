a9cfb9d8375d364010720befc5d530e8a6de9795
Darlington6 <d.tunyinko@alustudent.com>
2024-03-23 05:03:00 -0700

    Commit 5

--- python-network_1/5-hbtn_header.py
+++ python-network_1/5-hbtn_header.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""This is documented now."""
+"""Documented now"""
 import requests
 import sys
 
 if __name__ == '__main__':
     url = sys.argv[1]
     response = requests.get(url)
-    print("{}".format(response.headers.get("X-Request-Id)))
+    print("{}".format(response.headers.get("X-Request-Id")))
