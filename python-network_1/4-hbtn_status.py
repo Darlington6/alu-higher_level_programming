0c87c81d55b27c4d3d8fb0d7ee109e866380b9b8
Darlington6 <d.tunyinko@alustudent.com>
2024-03-23 06:03:43 -0700

    Commit 4

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """Documented now"""
+
 import requests
 
 if __name__ == '__main__':
-    response = requests.get("https://intranet.hbtn.io/status")
+    url = "https://intranet.hbtn.io/status"
+    r = requests.get(url)
+    text = r.text
     print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+    print("\t- type: {}".format(type(text)))
+    print("\t- content: {}".format(text))
