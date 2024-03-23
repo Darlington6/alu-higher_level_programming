3463273a5ad76d837b14e50484843d4418c9310e
Darlington6 <d.tunyinko@alustudent.com>
2024-03-23 03:10:39 -0700

    Commit 2

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -6,11 +6,11 @@ import urllib.parse
 import sys
 
 if __name__ == '__main__':
-    """This is documented."""
+    """"This is documented"""
     url = sys.argv[1]
     values = {"email": sys.argv[2]}
     data = urllib.parse.urlencode(values)
-    data = data.encode('ascii') # Data should be in bytes.
+    data = data.encode('ascii')  # Data should be in bytes
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         content = response.read()
